5eb568f5cba3fbd06fd96985c08f39b72d49b441
chenlei <381965081@qq.com>
2025-01-06 17:15:34 +0800

    init

--- src/main/java/com/cl/mongo/controller/MongoQueryController.java
+++ src/main/java/com/cl/mongo/controller/MongoQueryController.java
@@ -1,9 +1,23 @@
 package com.cl.mongo.controller;
 
+import com.cl.mongo.query.AtmMongoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class MongoQueryController {
 
+    @Autowired
+    private AtmMongoServiceImpl atmMongoServiceImpl;
+
+    @RequestMapping("/query")
+    public List query(@RequestParam("startDate")Date startDate, @RequestParam("endDate")Date endDate, @RequestParam("devId")String devId){
+        return atmMongoServiceImpl.query(startDate,endDate,devId);
+    }
 
 }
